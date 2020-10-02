fe0448809bf6e9e52255ca00dc8d3c60c40e83f7
Alexander Römer <alex@roemer.design>
2020-10-02 11:23:37 +0200

    Initial Commit

---
+++ TicTacaToe/TicTacaToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacaToeApp.swift
+//  TicTacaToe
+//
+//  Created by Alexander Römer on 02.10.20.
+//
+
+import SwiftUI
+
+@main
+struct TicTacaToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
